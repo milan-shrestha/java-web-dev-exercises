e079fe67f7a4e79dfd8250844e7098be3279cfe0
Milan Shrestha <milanshrestha17@hotmail.com>
2020-08-21 10:40:36 -0400

    Completed bonus mission

--- src/org/launchcode/java/studios/charactercount/CharacterCount.java
+++ src/org/launchcode/java/studios/charactercount/CharacterCount.java
@@ -2,31 +2,34 @@ package org.launchcode.java.studios.charactercount;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class CharacterCount {
 
     public static void main(String[] args) {
 
-        String myString = "If the product of two terms is zero then common sense says at least one of the two terms has" +
-                            " to be zero to start with. So if you move all the terms over to one side, you can put the quadratics " +
-                            "into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done " +
-                            "that, it’s pretty straightforward from there.";
+        Scanner input = new Scanner(System.in);                 //Declare Scanner type variable to get user input
+        System.out.println("Enter a word for character count: ");
 
-        char[] charactersInString = myString.toCharArray();
+        String userInput = input.next().toLowerCase();          //Convert userInput to lowercase to mae it case-insensitive
 
-        HashMap<Character, Integer> count = new HashMap<>();
+        char[] charactersInString = userInput.toCharArray();    //Convert userInput to an array of characters
 
-        for (char c : charactersInString) {
-            if (count.containsKey(c)) {
-                int existCount = count.get(c);
-                count.put(c, existCount + 1);
-            } else {
-                count.put(c, 1);
+        HashMap<Character, Integer> count = new HashMap<>();    //Use HashMap and declare variable of count
+
+        for (char c : charactersInString) {                     //Loop through array for each characters
+            if (c >= 'a' && c <= 'z') {                         //Check if the characters are alphabets
+                if (count.containsKey(c)) {                     //Check if current character already exists
+                    int existCount = count.get(c);              //If it exists; get the current value associated to it
+                    count.put(c, existCount + 1);               //Increment the value for the character by 1 and put it back
+                } else {
+                    count.put(c, 1);                            //If false; put the value of the character as 1
+                    }
+                }
             }
-        }
 
-        for (Map.Entry<Character, Integer> charCount : count.entrySet()) {
-            System.out.println(charCount.getKey() + " : " + charCount.getValue());
+        for (Map.Entry<Character, Integer> charCount : count.entrySet()) {          //Map through the entries in count
+            System.out.println(charCount.getKey() + " : " + charCount.getValue());  //Print the result in 'key-value' pairs
         }
     }
 }
