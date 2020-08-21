8f256b0fb5df6cfadc47665e96df0ffc049a1ab0
Milan Shrestha <milanshrestha17@hotmail.com>
2020-08-21 11:12:08 -0400

    Cleaned up codes to line up comments evenly

--- src/org/launchcode/java/studios/charactercount/CharacterCount.java
+++ src/org/launchcode/java/studios/charactercount/CharacterCount.java
@@ -10,31 +10,31 @@ public class CharacterCount {
 
     public static void main(String[] args) {
 
-//        Scanner input = new Scanner(System.in);                 //Declare Scanner type variable to get user input
+//        Scanner input = new Scanner(System.in);                               //Declare Scanner type variable to get user input
 //        System.out.println("Enter a word for character count: ");
 
         //Super-bonus mission
-        String text = "";                                               //Declare text variable of String type
-        try {                                                           //Use exception handling for file reading
-            Scanner input = new Scanner(new File("text.txt")); //Use Scanner to read text from external file
+        String text = "";                                                       //Declare text variable of String type
+        try {                                                                   //Use exception handling for file reading
+            Scanner input = new Scanner(new File("text.txt"));         //Use Scanner to read text from external file
             text = input.nextLine();
-        } catch (FileNotFoundException e) {                             //Throw error if file not found
+        } catch (FileNotFoundException e) {                                     //Throw error if file not found
             e.printStackTrace();
         }
 
-        text = text.toLowerCase();          //Convert userInput to lowercase to mae it case-insensitive
+        text = text.toLowerCase();                                              //Convert userInput to lowercase to mae it case-insensitive
 
-        char[] charactersInString = text.toCharArray();    //Convert userInput to an array of characters
+        char[] charactersInString = text.toCharArray();                         //Convert userInput to an array of characters
 
-        HashMap<Character, Integer> count = new HashMap<>();    //Use HashMap and declare variable of count
+        HashMap<Character, Integer> count = new HashMap<>();                    //Use HashMap and declare variable of count
 
-        for (char c : charactersInString) {                     //Loop through array for each characters
-            if (c >= 'a' && c <= 'z') {                         //Check if the characters are alphabets
-                if (count.containsKey(c)) {                     //Check if current character already exists
-                    int existCount = count.get(c);              //If it exists; get the current value associated to it
-                    count.put(c, existCount + 1);               //Increment the value for the character by 1 and put it back
+        for (char c : charactersInString) {                                     //Loop through array for each characters
+            if (c >= 'a' && c <= 'z') {                                         //Check if the characters are alphabets
+                if (count.containsKey(c)) {                                     //Check if current character already exists
+                    int existCount = count.get(c);                              //If it exists; get the current value associated to it
+                    count.put(c, existCount + 1);                               //Increment the value for the character by 1 and put it back
                 } else {
-                    count.put(c, 1);                            //If false; put the value of the character as 1
+                    count.put(c, 1);                                            //If false; put the value of the character as 1
                     }
                 }
             }
